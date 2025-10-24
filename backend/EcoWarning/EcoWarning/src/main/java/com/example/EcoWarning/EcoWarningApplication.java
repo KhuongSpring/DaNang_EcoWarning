1d86f9a5a430f05304f9a545fe761bea27bc52dc
khuong1742005 <boyzsno1@gmail.com>
2025-10-24 13:52:45 +0700

    feat: init project

---
+++ backend/EcoWarning/EcoWarning/src/main/java/com/example/EcoWarning/EcoWarningApplication.java
@@ -0,0 +1,13 @@
+package com.example.EcoWarning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoWarningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoWarningApplication.class, args);
+	}
+
+}
---
+++ backend/EcoWarning/EcoWarning/src/test/java/com/example/EcoWarning/EcoWarningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EcoWarning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoWarningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
